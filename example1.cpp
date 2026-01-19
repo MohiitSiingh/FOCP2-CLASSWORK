f2d33c2740fb3bcb29f753ec9c7e4f7d106793bb
Mohit <mohiitsiingh10@gmail.com>
2026-01-19 11:06:08 +0530

    Initial commit

---
+++ example1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+    int marks1,marks2,marks3,total;
+    std::cout<<"enter: ";
+    std::cin>>marks1>>marks2>>marks3;
+    std::cout<<"marks are: "<<marks1<<"\n";
+    std::cout<<"marks are: "<<marks2<<"\n";
+    std::cout<<"marks are: "<<marks3<<std::endl;
+    total=marks1+marks2+marks3;
+    std::cout<<"total marks are: "<<marks1+marks2+marks3;
+    //std::cout<<"total marks are: "<<total;
+    return 0 ;
+}
---
+++ prog1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+// using namespace std;
+int main() {
+   /* int rno;
+    //printf(" ");
+    std::cout<<"Enter rno: ";
+//for \n it has <<endl<<
+
+    //scanf(" ");
+    std::cin>>rno;
+
+    //printf(" ",rno);
+    std::cout<<"your rno is: "<<rno;
+    */
+
+    int rno,m1,m2,m3;
+    //printf(" ");
+    std::cout<<"Enter rno: ";
+//for \n it has <<endl<<
+
+    //scanf(" ");
+    std::cin>>rno>>m1>>m2>>m3;
+
+    //printf(" ",rno);
+    std::cout<<"your rno is: "<<rno<<std::endl<<m1<<"\n"<<m2<<std::endl<<m3;
+    return 0;
+}
