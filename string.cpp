0349049e7a7584caa93ecfd3fc83a1e0e6852d51
Mohit <mohiitsiingh10@gmail.com>
2026-01-20 07:34:56 +0000

    Jan 20, 2026, 1:04 PM

--- string.cpp
+++ string.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 int main(){
  std::string name;
- std::cout<<"enter";
-// std::cin>>name; //it will take space as enter
+ std::cout<<"enter ";
+std::cin>>name; //it will take space as enter
  // std::cout<<"name is "<<name; 
- //getline(std::cin,name);
- //std::cout<<"name is "<<name;
+ std::cin ignore();
+ getline(std::cin,name);
+ std::cout<<"name is "<<name;
  
     return 0;
 }
