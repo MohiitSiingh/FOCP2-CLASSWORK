0162d011c00b25d1767e158a7d7055d5858b221f
Mohit <mohiitsiingh10@gmail.com>
2026-02-02 03:28:04 +0000

    Feb 2, 2026, 8:58 AM

--- string.cpp
+++ string.cpp
@@ -4,10 +4,12 @@ int main(){
  int n;
  std::cout<<"enter ";
 std::cin>>n; //it will take space as enter
-  std::cout<<"n is "<<n; 
- std::cin ignore();
+  std::cout<<"n is \n"<<n;
+ std::cin.ignore();//so after enetring value of 9 it should not take enter
+ //  as value of string
  getline(std::cin,name);
+  
  std::cout<<"\nname is "<<name;
- 
+
     return 0;
 }
