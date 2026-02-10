96c9863daac9f145af84bcb73d48a0afcc31b060
Mohit <mohiitsiingh10@gmail.com>
2026-02-10 08:19:53 +0000

    Feb 10, 2026, 1:49 PM

--- swapfunction.cpp
+++ swapfunction.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 //void swap (int &r1,int &r2);
 int swap(int a,int b);
@@ -36,4 +36,23 @@ int swap(int a,int b){
     a=a-b;
     return b;
   //  cout<<"values of a and b after swap: "<<a<<"\n"<<b;
+}*/
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) ;
+
+int main() {
+    int a = 23, b = 9;
+    cout << "Before swap: a = " << a << ", b = " << b << endl;
+
+    swap(a, b);
+
+    cout << "After swap: a = " << a << ", b = " << b << endl;
+    return 0;
 }
+void swap(int &a, int &b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}//pass by value pass by reference pass by address or pointer
