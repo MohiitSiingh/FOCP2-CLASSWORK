0edd8b08be6345ab7413d534b053636d7a80dec6
Mohit <mohiitsiingh10@gmail.com>
2026-02-23 03:34:55 +0000

    Feb 23, 2026, 9:04 AM

--- class.cpp
+++ class.cpp
@@ -1,1 +1,22 @@
 #include<iostream>
+#include<string>
+using namespace std;
+class solution{ 
+    int rollNo;
+    string name;
+    int marks1;
+    int marks2;
+    int marks3;
+void inputDetails(){//setter
+    cout<<"\n Enter Rollno,name,marks1,marks2 &marks3";
+    cin>>rollNo>>name>>marks1>>marks2>>marks3;
+}
+int calculateTotal(){//getter
+    return (marks1+marks2+marks3);
+}
+void displayDetails(){
+    cout<<"\n Name : "<<name<<"\n Rollno : "<<rollNo;
+    cout<<"\n Marks1: "<<marks1<<"\n Marks2 : "<<marks2<<"\n Marks3 : "<<marks3;
+    cout<<"\n Total = "<<calculateTotal();
+}
+};
