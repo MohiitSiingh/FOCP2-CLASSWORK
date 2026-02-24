c36bd9dfec7f24e048acb1170134713e956cf673
Mohit <mohiitsiingh10@gmail.com>
2026-02-24 07:58:04 +0000

    Feb 24, 2026, 1:28 PM

--- classrectangle.cpp
+++ classrectangle.cpp
@@ -1,43 +1,54 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-class Rectangle {  
-    private:
-float length;
-float width;
+
+class Rectangle {
+private:
+    float length;
+    float width;
+
 public:
-//if want to end }; class like this you can only declare them in class reactangle and 
-//use void Reactangle::displayResult in this way you can identify curly bracket in big code
-void inputDimensions(){
-    cout<<"\n Enter length and Enter width: ";
-cin>>length>>width;
-}
-Rectangle();
-Reactangle(int,int);
-float calculateArea(){
-    return (length*width);
-}
-float Rectangle::calculatePerimeter(){
-    return (2*(length+width));
-}
-void Rectangle::displayResult(){
-cout<<"\n Area is "<<calculateArea();
-    cout<<"\n perimeter is "<<calculatePerimeter();
-}
+    void inputDimensions() {
+        cout << "\nEnter length and width: ";
+        cin >> length >> width;
+    }
+
+    Rectangle(); // default constructor
+    Rectangle(int, int); // parameterized constructor
+
+    float calculateArea() {
+        return (length * width);
+    }
+
+    float calculatePerimeter() {
+        return (2 * (length + width));
+    }
+
+    void displayResult() {
+        cout << "\nArea is " << calculateArea();
+        cout << "\nPerimeter is " << calculatePerimeter();
+    }
 };
-Rectangle::Rectangle(){
-    cout<<"defualt";
-    length=0;
-    width=0;
+
+// Default constructor
+Rectangle::Rectangle() {
+    cout << "Default constructor called\n";
+    length = 0;
+    width = 0;
 }
-Rectangle::Rectangle(int l,int w){
-    length=l;
-    width=w;
+
+// Parameterized constructor
+Rectangle::Rectangle(int l, int w) {
+    length = l;
+    width = w;
 }
-int main(){
-Rectangle r1,r2(78,45);
-   // r1.inputDimensions();
+
+int main() {
+    Rectangle r1, r2(78, 45);
+
+    // r1.inputDimensions(); // Uncomment if you want user input
     r1.displayResult();
-      r2.displayResult();
+    r2.displayResult();
+
     return 0;
 }
