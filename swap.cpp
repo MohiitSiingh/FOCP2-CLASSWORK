7fa8508c9696855da32576d1c2256ae00320b859
Mohit <mohiitsiingh10@gmail.com>
2026-03-02 03:33:28 +0000

    Mar 2, 2026, 9:03 AM

---
+++ swap.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void Swap(int,int);
+void Swap(float,float);
+int main(){
+int a=10;
+int b=8;
+//swap(10,8);
+cout<<"swap int values: "<<swap(10,8);
+cout<<"swap int values: "<<swap(10.23,8.45);
+    return 0;
+}
+swap(a,b){
+    c=a;
+    a=b;
+    b=c;
+    return a,b;
+}
+swap(a,b){
+    c=a;
+    a=b;
+    b=c;
+    return a,b;
+}
