0c51583d89c45f08c599d3b408570b083d9f13cd
Mohit <mohiitsiingh10@gmail.com>
2026-03-02 04:03:36 +0000

    Mar 2, 2026, 9:33 AM

--- swap.cpp
+++ swap.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void swap(int , int );
+void swap(float,float);
+int main(){
+int a =10,b=8;
+float x = 3.3,y=7.3;
+cout<<"before swap : "<<a<<b;
+swap(10,8);
+cout<<"before swap : "<<x<<y;
+swap(3.3f,7.3f);
+return 0;
+}
+swap( a,int b){
+    int temp = a;
+    a=b;
+    b=temp;
+    cout<<"after swap: "<<a<<b;
 
+}
+swap(float x,float y){
+    float temp = x;
+    x=y;
+    y=temp;
+    cout<<"after swap: "<<x<<y;
+}
