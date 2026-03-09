f30bca9b828bee8f6329928436ade13ed2bbbf20
Mohit <mohiitsiingh10@gmail.com>
2026-03-09 04:58:35 +0000

    Mar 9, 2026, 10:28 AM

---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Student{
+    int rno;
+    string name;
+    float m1;
+    public:
+    Student();
+    Student(int,string,float);
+    void display();
+    
+}
+int main(){
+
+}
