6287f6e6c7a17c5a6ed8f7a250651241ee6fc739
Mohit <mohiitsiingh10@gmail.com>
2026-03-17 18:11:10 +0000

    Mar 17, 2026, 11:41 PM

--- ques3class.cpp
+++ ques3class.cpp
@@ -1,4 +1,16 @@
 #include<iostream>
 #include<string>
 using namespace std;
-/* */
+/* Q3. Write a C++ program to create a class Rectangle with length and breadth
+ as data members.
+Use a constructor to initialize the dimensions and calculate the area of the rectangle.*/
+class Rectangle{
+private:
+float length;
+float breadth;
+public:
+Rectangle(float l,float b){
+    length = l;
+    breadth = b;
+}
+};
