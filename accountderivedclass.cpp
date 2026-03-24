19fde031e9c200cf6cc6491387dbc41234cbcb84
Mohit <mohiitsiingh10@gmail.com>
2026-03-24 07:29:02 +0000

    Mar 24, 2026, 12:59 PM

--- accountderivedclass.cpp
+++ accountderivedclass.cpp
@@ -1,2 +1,6 @@
 #include<iostream>
-/*  */
+/* Create a class Account with account number and balance.
+ Add a function displayAccount().
+  Derive a class SavingsAccount that includes interest rate and a
+   function calculateInterest().
+   Demonstrate usage of both classes. */
