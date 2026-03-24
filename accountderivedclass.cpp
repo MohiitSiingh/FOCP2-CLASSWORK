4673e56c58d5c259b7cd2bfd503fd23f99d5d285
Mohit <mohiitsiingh10@gmail.com>
2026-03-24 07:39:01 +0000

    Mar 24, 2026, 1:09 PM

--- accountderivedclass.cpp
+++ accountderivedclass.cpp
@@ -1,12 +1,30 @@
 #include<iostream>
+usinf namespace std;
 /* Create a class Account with account number and balance.
  Add a function displayAccount().
   Derive a class SavingsAccount that includes interest rate and a
    function calculateInterest().
    Demonstrate usage of both classes. */
-   class Account {
+   class Account{
 int acc_num;
 float balance;
 public:
-int 
-   };
+int get_accnum(){
+    return acc_num;
+}
+void set_accnum(int acc_num)
+    {this->acc_num = acc_num;
+
+}
+int getbalance(){
+    return balance;
+}
+void setbalance(float balance){
+    this->balance = balance;
+}
+void displayAccount(){
+    cout<<"account number ="<<acc_num;
+    cout<<"\nbalance ="<<balance;
+}  
+
+ };
