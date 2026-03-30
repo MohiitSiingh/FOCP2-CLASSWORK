f31147dbc2df9a857cd7422ca10ee88e82557780
Mohit <mohiitsiingh10@gmail.com>
2026-03-30 04:17:43 +0000

    Mar 30, 2026, 9:47 AM

--- inheritence_hierarchy.cpp
+++ inheritence_hierarchy.cpp
@@ -2,7 +2,19 @@
 #include<string>
 using namespace std;
 class A {
-
+string name;
+int age;
+public:
+void display(){
+    cout<<"\n name = "<<name<<"\n age = "<<age;
+}
+A(){
+    cout<<"\n person default ";name="";age=18;
+}
+A( string name , int age)
+cout<<"person parameterized";
+this->name=name;
+this->age=age;
 
 };
 class B : public A{
