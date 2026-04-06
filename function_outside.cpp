eee974b3c7eff82b3bb4825634cc60b2404bc791
Mohit <mohiitsiingh10@gmail.com>
2026-04-06 03:40:21 +0000

    Apr 6, 2026, 9:10 AM

--- function_outside.cpp
+++ function_outside.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
-using namespace std
+using namespace std;
 class Rectangle{
-
+int length;
+int width;
+public:
+Rectangle(){
+    length = 0; width =0;
+}
+Rectangle(int l , int w){
+    length = l;width = w;
+}
+void display(){
+    cout<<"\n lenght = "<<length;
+    cout<<"\n width = "<<width;
+}
+friend void area (Rectangle obj);
 };
+void area (Rectangle obj){
+    int area = obj.length*obj.width;
+    cout<<"\n Area = "<<area; 
+}
+int main(){
+    Rectangle r1,r2(10,15);
+    r1.display();
+    r2.display();
+    area (r2);
+    return 0;
+}
