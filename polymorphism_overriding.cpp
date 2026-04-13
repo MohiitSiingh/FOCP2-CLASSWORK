b50a101d0d851fb3a45dc4ee71d06eefdfa18691
Mohit <mohiitsiingh10@gmail.com>
2026-04-13 04:42:59 +0000

    Apr 13, 2026, 10:12 AM

---
+++ polymorphism_overriding.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class shape{
+    public:
+    shape(){
+        cout<<"\n shape default const called";}
+        void area(){
+            cout<<"\n no dim no area";
+        }
+void display(){
+    cout"\n we are 2d shapes";
+}
+    
+};
+class Rectangle:public shape{
+    int length;int width;
+    public:
+    Rectangle(int l = 5, int w = 10 ): length(l),width(w){
+        cout<<"\n Rectangle object created";
+    }
+    void area(){
+        cout<<"\n area = "<<length*width;
+    }
+};
+int main(){
+    shape s1;
+    Rectangle r2(50,45);
+    s1.area();
+    r2.area();
+    r2.shape::area();
+    s1.display();
+    r2.display();
+    return 0;
+}
