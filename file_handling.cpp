6db73e491f7022265abf9f1879d7e66d84e8a93e
Mohit <mohiitsiingh10@gmail.com>
2026-04-20 04:11:46 +0000

    Apr 20, 2026, 9:41 AM

--- file_handling.cpp
+++ file_handling.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 
 int main(){
 fstream fout("data.txt",ios::out);
 fout << "This is file handling in c++\n";
 fout<< "this is file handling in c++ where ios::out truncates the file";
-fout.close();
+ 
 cout<< "data written successfully";
     return 0;
 }
