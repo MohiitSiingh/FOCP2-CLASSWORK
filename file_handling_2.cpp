8044d2bf97c9a16facc9eba4729d25848ca7d679
Mohit <mohiitsiingh10@gmail.com>
2026-04-20 04:48:52 +0000

    Apr 20, 2026, 10:18 AM

--- file_handling_2.cpp
+++ file_handling_2.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 int main (){
 ifstream fin("./data.txt");
-
-
+  fstream fout("copydata.txt",ios::app);
+string line;
+while(getline(fin , line )){
+  
+    fout << line << endl;
+  
+}
+  fout.close();
+fin.close();
 }
