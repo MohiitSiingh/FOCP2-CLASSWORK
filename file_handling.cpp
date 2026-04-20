b25642db21f542fd5be713f552ffa1e3cd8ac7c7
Mohit <mohiitsiingh10@gmail.com>
2026-04-20 03:44:06 +0000

    Apr 20, 2026, 9:14 AM

--- file_handling.cpp
+++ file_handling.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-ofstream fout("data.txt");
+fstream fout("data.txt",ios::out);
 fout << "This is file handling in c++\n";
 fout<< "the syntax is different";
 fout.close();
