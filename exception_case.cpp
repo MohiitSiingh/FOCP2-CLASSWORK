b9e7a404098d982635abcedd03fa90d817c34c53
Mohit <mohiitsiingh10@gmail.com>
2026-04-21 08:07:40 +0000

    Apr 21, 2026, 1:37 PM

---
+++ exception_case.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<exception>
+using namespace std;
+
+int main(){
+int a =10,b=0;
+
+try{
+    if(b ==0)
+    throw "division by zero error!";
+}
+cout<< "Result : "<<a / b << endl;
+
+}
+catch(const char* msg){
+    cout << 
+}
