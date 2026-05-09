8bd9fa698dfe49b4c0f2b742026c7e74a2c67ab9
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 12:46:06 +0000

    May 9, 2026, 6:16 PM

--- P5.cpp
+++ P5.cpp
@@ -1,3 +1,12 @@
 // Q5. Define a class BankAccount with account number and balance.
 //  Use a constructor to set 
 // the initial balance and write a function to display account details. 
+#include<iostream>
+#include<string>
+using namespace std;
+class Bankaccount{
+string acc_num;
+float balance;
+public:
+
+}
