099db8d0678270460b250f189f6b75d3a5d81ab7
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 11:46:37 +0000

    May 9, 2026, 5:16 PM

---
+++ Prac1.cpp
@@ -0,0 +1,4 @@
+// Q1. Create a class Student with data members: name and marks. 
+// Use a constructor to initialize 
+// the values and display the student details using a member function. 
+#include
