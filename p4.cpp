6cb387de82e25060fce8e6a1025aa86cc42b904c
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 12:36:21 +0000

    May 9, 2026, 6:06 PM

--- p4.cpp
+++ p4.cpp
@@ -1,4 +1,22 @@
 // Q4. Create a class Employee with data members: emp_id and salary.
 //  Initialize the values using 
 // a parameterized constructor and display the employee information. 
-#include<
+#include<iostream>
+#include<string>
+using namespace std;
+class Employee{
+    string emp_id;
+    float salary;
+    public :
+    Employee(string id , float sal){
+        emp_id = id;
+        salary = sal;
+    }
+    void displayDetails(){
+        cout<<"employee id : "<<emp_id<<"\nSalary = "<<salary;
+    }
+};
+int main(){
+Employee E("E121",10000);
+E.displayDetails();
+}
