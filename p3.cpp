8e2e27f2127708e2cc973350c23dd0e096ad7941
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 12:20:40 +0000

    May 9, 2026, 5:50 PM

--- p3.cpp
+++ p3.cpp
@@ -6,5 +6,18 @@
 #include<string>
 using namespace std;
 class Rectangle{
-
+int length,breadth;
+public:
+Rectangle(int l , int b){
+    length = l;
+    breadth = b;
+}
+int calculateArea(){
+    return (length*breadth);
+}
 };
+int main(){
+    Rectangle r1(13,4);
+   cout<<"Area of this rectangle is "<< r1.calculateArea();
+    return 0;
+}
