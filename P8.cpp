9311aef5a69ffeab4c1c15ed6d9ff7ddf78beed4
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 16:14:45 +0000

    May 9, 2026, 9:44 PM

---
+++ P8.cpp
@@ -0,0 +1,7 @@
+// Q.8 Write a C++ program to demonstrate function overloading by creating multiple functions 
+// named volume: 
+// • One function to calculate the volume of a cube (side) 
+// • One function to calculate the volume of a cuboid (length, breadth, height) 
+// • One function to calculate the volume of a cylinder (radius, height) 
+// Call all the overloaded volume functions in the main() function and display the calculated 
+// volumes.
