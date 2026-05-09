614894f0b49831365803435c4af040f6d60641d5
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 16:50:55 +0000

    May 9, 2026, 10:20 PM

---
+++ P9.cpp
@@ -0,0 +1,22 @@
+// Q.9 Electricity Billing System (Function Overloading) 
+// An electricity board wants to design a billing system that calculates electricity
+//  charges for 
+// different types of consumers. The billing rules are: 
+// • If only units consumed are provided → calculate bill at ₹5 per unit. 
+// • If units + rate per unit are provided → calculate accordingly. 
+// • If units + rate + fixed charge are provided → add fixed charge to total bill. 
+// Requirements: 
+// 1. Create a class ElectricityBill. 
+// 2. Implement function overloading for a function named calculateBill(): 
+// I. 
+// calculateBill(int units) 
+// II. 
+// III. 
+// calculateBill(int units, float rate) 
+// calculateBill(int units, float rate, float fixedCharge) 
+// 3. Display the calculated bill in each case. 
+// 4. In main(): Call all three overloaded functions using different arguments. 
+#include<iostream>
+#include<string>
+using namespace std;
+class 
