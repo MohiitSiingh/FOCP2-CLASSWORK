efaecca1c2b1ab875424c9a699e13ca0031e5277
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 12:10:39 +0000

    May 9, 2026, 5:40 PM

--- p3.cpp
+++ p3.cpp
@@ -1,2 +1,10 @@
-Q3. Write a C++ program to create a class Rectangle with length and breadth as data members. 
-Use a constructor to initialize the dimensions and calculate the area of the rectangle.
+// Q3. Write a C++ program to create a class Rectangle
+//  with length and breadth as data members. 
+// Use a constructor to initialize the dimensions and
+//  calculate the area of the rectangle.
+#include<iostream>
+#include<string>
+using namespace std;
+class Reactangle{
+
+};
