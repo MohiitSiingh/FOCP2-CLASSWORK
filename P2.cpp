fb74b41caa43cca691fc6f1481687e7a01683237
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 12:08:51 +0000

    May 9, 2026, 5:38 PM

--- P2.cpp
+++ P2.cpp
@@ -8,10 +8,21 @@ class Car{
 string brand;
 float price;
 public:
-Car(string b,float p){
-brand = b;
-price = p;
+Car(){
+brand = "maruti";
+price = 150000;
 
 }
-Car(string )
+Car(string b,float p) {
+    brand = b;
+    price = p;
+}
+void display(){
+    cout<<"\nBrand is : "<<brand<<"\nprice is: "<<price;
+}
 };
+int main(){
+    Car c1,c2("Toyota",200000);
+    c1.display();
+    c2.display();
+}
---
+++ p3.cpp
@@ -0,0 +1,2 @@
+Q3. Write a C++ program to create a class Rectangle with length and breadth as data members. 
+Use a constructor to initialize the dimensions and calculate the area of the rectangle.
