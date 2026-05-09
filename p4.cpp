c81fc58f333a3a422b12095ef23f5971f53508d0
Mohit <mohiitsiingh10@gmail.com>
2026-05-09 12:27:57 +0000

    May 9, 2026, 5:57 PM

--- p4.cpp
+++ p4.cpp
@@ -1,3 +1,4 @@
 // Q4. Create a class Employee with data members: emp_id and salary.
 //  Initialize the values using 
 // a parameterized constructor and display the employee information. 
+#include<
