c3d8e8eab60013feb2dcdc51afa16beb232e4d62
Mohit <mohiitsiingh10@gmail.com>
2026-05-10 07:08:04 +0000

    May 10, 2026, 12:38 PM

---
+++ P10.cpp
@@ -0,0 +1,13 @@
+// Q.10 Online Shopping Cart 
+// An e-commerce website wants to represent items in a user's shopping cart. 
+// Each item should contain: 
+// • Product name 
+// • Price 
+// • Quantity 
+// Questions 
+// 1. Create a class called Product. 
+// 2. Use a constructor to initialize product details. 
+// 3. Write a function to calculate total cost = price × quantity. 
+// 4. Create two objects for different products. 
+// 5. When the program ends, the destructor should display a message indicating that the 
+// product is removed from memory.
