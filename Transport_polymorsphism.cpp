1ece46de19b74303746ad55f23c1cbdf6716b13e
Mohit <mohiitsiingh10@gmail.com>
2026-05-12 09:54:50 +0000

    May 12, 2026, 3:24 PM

--- Transport_polymorsphism.cpp
+++ Transport_polymorsphism.cpp
@@ -5,3 +5,35 @@
 #include<iostream>
 #include<string>
 using namespace std;
+class Vehicle{
+    public:
+    virtual float fuelEfficiency(){
+cout<<"choose a vehicle: \n";
+return 0;
+    }
+};
+class Car:public Vehicle{
+    public:
+    float fuel_used,km_run;
+    Car(float fu,float km){
+        fuel_used=fu;
+        km_run=km;
+    }
+        float fuelEfficiency() override{
+            return km_run/fuel_used;
+        }
+};
+class Bike:public Vehicle{
+    public:
+    float fuel_used,km_run;
+    Bike(float fu,float km){
+        fuel_used=fu;
+        km_run=km;
+    }
+        float fuelEfficiency() override{
+            return km_run/fuel_used;
+        }
+};
+int main(){
+    
+}
