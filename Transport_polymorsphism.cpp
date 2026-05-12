183cf92334c43c7bed0e9617338fe94e63a060d5
Mohit <mohiitsiingh10@gmail.com>
2026-05-12 09:48:33 +0000

    May 12, 2026, 3:18 PM

---
+++ Transport_polymorsphism.cpp
@@ -0,0 +1,7 @@
+// Transport System  
+// Base class Vehicle with virtual function fuelEfficiency().
+//  Derive Car and Bike classes, override efficiency calculation, and
+//  display results using base class pointer
+#include<iostream>
+#include<string>
+using namespace std;
