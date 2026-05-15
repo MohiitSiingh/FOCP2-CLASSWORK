a2688b70999ed767f7941360718b596aadd3dabd
Mohit <mohiitsiingh10@gmail.com>
2026-05-15 14:46:45 +0000

    May 15, 2026, 8:16 PM

--- movie_ticket.cpp
+++ movie_ticket.cpp
@@ -2,6 +2,7 @@
 #include<string>
 using namespace std;
 class movie_ticket{
+    int slots = 100;
 //     • BOOK X Y  
 // o Allow a customer with a unique ID X to book a ticket for a movie with the movie ID Y.  
 // o If the customer has already booked a ticket for the same movie or the movie is sold out, 
